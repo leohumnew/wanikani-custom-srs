9e58a2fc65e78c9be5635c8e3bc26e52e752a2c0
Leo <39741041+leohumnew@users.noreply.github.com>
2024-03-05 23:55:10 +0000

    Improve Dashboard
    - Intercept calls to review count endpoint so that review number in top nav bar are also updated
    - Fix dashboard selectors after WK update

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ if (window.location.pathname.includes("/review")) {
     const { fetch: originalFetch } = unsafeWindow;
     unsafeWindow.fetch = async (...args) => {
         let [resource, config] = args;
-        if (resource.includes("/subjects/review") && config && config.method === "POST") {
+        if (resource.includes("/subjects/review") && config != null && config.method === "post") {
             let payload = JSON.parse(config.body);
             // Check if submitted item is a custom item
             if(payload.counts && payload.counts[0].id < 0) {
@@ -32,7 +32,7 @@ if (window.location.pathname.includes("/review")) {
             } else {
                 return originalFetch(...args);
             }
-        } else if (resource.includes("/subject_info/") && config && config.method === "GET") {
+        } else if (resource.includes("/subject_info/") && config && config.method === "get") {
             let subjectId = resource.split("/").pop();
             let subjectInfo = activePackProfile.getSubjectInfo(subjectId);
             console.log(subjectInfo);
@@ -41,12 +41,46 @@ if (window.location.pathname.includes("/review")) {
             return originalFetch(...args);
         }
     };
+
 // ----------- If on lessons page -----------
 } else if (window.location.pathname.includes("/lessons")) {
     // TODO
+
+// ----------- If on dashboard page -----------
 } else if (window.location.pathname.includes("/dashboard") || window.location.pathname === "/") {
+    // Catch lesson / review count fetch and update it with custom item count
+    const { fetch: originalFetch } = unsafeWindow;
+    unsafeWindow.fetch = async (...args) => {
+        let [resource, config] = args;
+        if (resource.includes("lesson-and-review-count") && config != null && config.method === "get") {
+            let response = await originalFetch(...args);
+            let data = await response.text();
+            data = parseHTML(data);
+
+            let reviewCountElement = data.querySelector("a[href='/subjects/review'] .lesson-and-review-count__count");
+            reviewCountElement.innerHTML = parseInt(reviewCountElement.innerHTML) + activePackProfile.getNumActiveReviews();
+
+            // Convert the DocumentFragment back to a string and return it as a Response
+            let serializedData = (new XMLSerializer()).serializeToString(data);
+            let res = new Response(serializedData, {
+                status: response.status,
+                headers: response.headers
+            });
+            return res;
+        } else {
+            return originalFetch(...args);
+        }
+    };
+    // Catch document load to edit review count on dashboard
     document.addEventListener("DOMContentLoaded", () => {
-        let reviewNumberElement = document.querySelector(".lessons-and-reviews__reviews-button .lessons-and-reviews__count span");
-        reviewNumberElement.innerHTML = activePackProfile.getNumActiveReviews() + parseInt(reviewNumberElement.innerHTML);
+        let reviewNumberElement = document.querySelector(".reviews-dashboard .reviews-dashboard__count-text span");
+        reviewNumberElement.innerHTML = parseInt(reviewNumberElement.innerHTML) + activePackProfile.getNumActiveReviews();
     });
+}
+
+// ----------- UTILITIES -----------
+function parseHTML(html) {
+    var t = document.createElement('template');
+    t.innerHTML = html;
+    return t.content;
 }

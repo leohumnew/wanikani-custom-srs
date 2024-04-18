ceae6791f9613a1b4673c09bf73585cb8b274e99
Leo <39741041+leohumnew@users.noreply.github.com>
2024-04-18 22:15:49 +0100

    0.5.3 - Fix for loadingScreenText

--- sync_manager.js
+++ sync_manager.js
@@ -41,6 +41,10 @@ class SyncManager {
         }
     }
 
+    static getLoadingScreenText() {
+        return unsafeWindow.document.getElementById("loadingScreenText").innerText;
+    }
+
     static getAccessToken(authCode) {
         let bodyTxt = `client_id=${encodeURIComponent(this.#c)}&` + atob("Y2xpZW50X3NlY3JldA==") +
         `=${encodeURIComponent(this.#s)}&` +
@@ -59,7 +63,7 @@ class SyncManager {
                 if(response.status != 200) {
                     Utils.log("Error getting access token");
                     Utils.log(response);
-                    window.document.getElementById("loadingScreenText").innerText = "Error getting access token";
+                    getLoadingScreenText().innerText = "Error getting access token";
                     return;
                 }
                 let responseJSON = JSON.parse(response.responseText);
@@ -72,7 +76,7 @@ class SyncManager {
             onerror: function(response) {
                 Utils.log("Error getting access token");
                 Utils.log(response);
-                window.document.getElementById("loadingScreenText").innerText = "Error getting access token";
+                getLoadingScreenText().innerText = "Error getting access token";
             }
         });
     }
@@ -87,9 +91,9 @@ class SyncManager {
             },
             onload: function(response) {
                 let files = JSON.parse(response.responseText).files;
-                if(!files || files.length == 0) window.document.getElementById("loadingScreenText").innerText = "Please close this window and then refresh WaniKani to begin syncing.";
+                if(!files || files.length == 0) getLoadingScreenText().innerText = "Please close this window and then refresh WaniKani to begin syncing.";
                 else {
-                    window.document.getElementById("loadingScreenText").innerHTML = `
+                    getLoadingScreenText().innerHTML = `
                     <h2>Select Source</h2>
                     <p style="font-size: 40%">Choose which version of your data to keep - whichever you choose will overwrite the other. Whicever you do not choose will be permanently deleted.</p>
                     <button style="width: 100%" id="driveButton">Google Drive<br><small>Last modified: ${new Date(files[0].modifiedTime).toLocaleString()}</small></button>
@@ -97,18 +101,18 @@ class SyncManager {
                     document.getElementById("driveButton").addEventListener("click", () => {
                         GM.setValue("customSrsDriveFileId", files[0].id);
                         SyncManager.loadDataFromDrive("main");
-                        window.document.getElementById("loadingScreenText").innerText = "Please close this window and then refresh WaniKani to begin syncing."
+                        getLoadingScreenText().innerText = "Please close this window and then refresh WaniKani to begin syncing."
                     });
                     document.getElementById("localButton").addEventListener("click", () => {
                         SyncManager.saveDataToDrive(packProfile, "main");
-                        window.document.getElementById("loadingScreenText").innerText = "Please close this window and then refresh WaniKani";
+                        getLoadingScreenText().innerText = "Please close this window and then refresh WaniKani";
                     });
                 }
             },
             onerror: function(response) {
                 Utils.log("Error getting file metadata from Google Drive");
                 Utils.log(response);
-                window.document.getElementById("loadingScreenText").innerText = "Error getting file metadata from Google Drive";
+                getLoadingScreenText().innerText = "Error getting file metadata from Google Drive";
             }
         });
     }
